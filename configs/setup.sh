b395d7a27da87f9814d2c3a7c3ee40a2adb84218
Seven <seven.zhan@outlook.com>
2026-04-12 17:30:34 +0800

    update configs

---
+++ configs/setup.sh
@@ -0,0 +1,11 @@
+# git alias
+git config --global alias.ad add
+git config --global alias.ck checkout
+git config --global alias.br branch
+git config --global alias.cm commit
+git config --global alias.st status
+git config --global alias.ph push
+git config --global alias.rb rebase
+git config --global alias.cp cherry-pick
+git config --global alias.pl "pull --rebase"
+git config --global alias.lg "log --oneline --graph --all"
